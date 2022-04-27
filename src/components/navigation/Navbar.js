3a8ff7ebfa3d130bd358e96ed220411be50527b7
Maja Garpelin <Majagarpelin@gmail.com>
2022-04-27 10:53:37 +0200

    getting started

---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      Irene Wickman Assistanshundar
+      <ul className='navlinks'>
+        <li><a href="#">Assistanshundar</a></li>
+        <li><a href="#">Filmer - assistanshundbeteenden</a></li>
+        <li><a href="#">Träning av assistanshundar</a></li>
+        <li><a href="#">Hunddagis och hundpensionat</a></li>
+        <li><a href="#">Om mig</a></li>
+        <li><a href="#">Kontakt</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
