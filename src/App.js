9c54f813d01f2b1f45c1443547f829299d282cb6
Maja Garpelin <Majagarpelin@gmail.com>
2022-04-27 11:40:31 +0200

    navbar footer videos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import Footer from './components/navigation/Footer';
 import Navbar from './components/navigation/Navbar';
 import Home from './views/Home';
+import Videos from './views/Videos';
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <Navbar />
       
       <Home />
+      <Videos />
+
+      <Footer />
     </div>
   );
 }
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,15 +3,18 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      Irene Wickman Assistanshundar
-      <ul className='navlinks'>
-        <li><a href="#">Assistanshundar</a></li>
-        <li><a href="#">Filmer - assistanshundbeteenden</a></li>
-        <li><a href="#">Träning av assistanshundar</a></li>
-        <li><a href="#">Hunddagis och hundpensionat</a></li>
-        <li><a href="#">Om mig</a></li>
-        <li><a href="#">Kontakt</a></li>
-      </ul>
+      <div className='navbar-container'>
+        <a className='logo-link' href="">Irene Wickman Assistanshundar</a>
+        <ul className='nav-links'>
+          <li><a href="#">Assistanshundar</a></li>
+          <li><a href="#">Filmer - assistanshundbeteenden</a></li>
+          <li><a href="#">Träning av assistanshundar</a></li>
+          <li><a href="#">Hunddagis och hundpensionat</a></li>
+          <li><a href="#">Om mig</a></li>
+          <li><a href="#">Kontakt</a></li>
+        </ul>
+      </div>
+      
     </div>
   )
 }
