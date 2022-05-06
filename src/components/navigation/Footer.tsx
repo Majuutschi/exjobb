9fd72a78b9f36454b2001d08ff0cbd916d20aeee
Maja Garpelin <Majagarpelin@gmail.com>
2022-05-06 14:37:20 +0200

    navbar, home and connect

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -4,7 +4,10 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-container'>
-        &copy; Irene Wickman Assistanshundar
+        <p>
+          &copy; Irene Wickman Assistanshundar
+        </p>
+        
         <div className='social'>
           <a href="https://www.facebook.com/Irene-Wickman-Assistanshundar-131872730169705/"><i className='fa-brands fa-facebook-f'></i></a>
         </div>
--- src/views/Connect.tsx
+++ src/views/Connect.tsx
@@ -24,7 +24,12 @@ const Connect = () => {
                 <label htmlFor="message">
                   Meddelande:
                 </label>
-                <textarea name="message" id="message" cols={30} rows={10}></textarea>
+                <textarea 
+                  name="message" 
+                  id="message" 
+                  cols={30} 
+                  rows={10}
+                ></textarea>
               </div>
               <div className='btn-center'>
                 <button className='btn'>Skicka</button>
@@ -33,12 +38,18 @@ const Connect = () => {
             </form> 
           </div>
           <div className='connect-details'>
-            <div>
-              <iframe className='connect-map' title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2019.2121775702024!2d16.091720716191602!3d59.59618059663961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465e7fd6a308fc37%3A0xab4117f1cf8472e6!2sIrene%20WickmanAssistanshundar!5e0!3m2!1ssv!2sse!4v1651478901489!5m2!1ssv!2sse" width="600" height="450" allowFullScreen loading="lazy" referrerPolicy='no-referrer-when-downgrade'></iframe>
+            <div className='connect-map-container'>
+              <iframe 
+                className='connect-map' 
+                title='map' 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2019.2121775702024!2d16.091720716191602!3d59.59618059663961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465e7fd6a308fc37%3A0xab4117f1cf8472e6!2sIrene%20WickmanAssistanshundar!5e0!3m2!1ssv!2sse!4v1651478901489!5m2!1ssv!2sse" 
+                allowFullScreen 
+                loading="lazy" 
+                referrerPolicy='no-referrer-when-downgrade'
+              ></iframe>
             </div>
-          </div>
-          <div className='connect-items'>
-              <div className='connect-item'>
+            <div className='connect-items'>
+              <div className='connect-item address'>
                 <p>Irene Wickman</p>
                 <p>Gårdesta 6</p>
                 <p>73192 Köping</p>
@@ -52,6 +63,7 @@ const Connect = () => {
                 <p>Swish: 1230867739</p>
               </div>  
             </div>
+          </div>
         </div>
         
         
